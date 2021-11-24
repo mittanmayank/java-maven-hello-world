29717b00a18c2956b5c667cf96d0fb6cbbe46f47
Shihaaz Buhary <60466231+sbuhary@users.noreply.github.com>
2021-11-24 23:05:24 +0530

    initial commit

---
+++ src/main/java/com/sample/java/maven/helloworld/java_maven_hello_world/App.java
@@ -0,0 +1,13 @@
+package com.sample.java.maven.helloworld.java_maven_hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
